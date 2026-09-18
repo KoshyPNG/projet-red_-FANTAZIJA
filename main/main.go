4a90dead9750e887818a1b4caf37196d779da585
KoshyPNG <catalinmonmail@gmail.com>
2026-09-18 16:28:32 +0200

    Character

---
+++ main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"projet_red"
+)
+
+func main() {
+	perso := projet_red.InitCharacter()
+	v := true
+	for v {
+		var i int
+		fmt.Println("clivk")
+		fmt.Scan(&i)
+		if i == 1{
+			projet_red.DisplayInfo(perso)
+			v = false
+		}
+	}
+}
