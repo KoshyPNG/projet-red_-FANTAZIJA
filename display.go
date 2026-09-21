7c4eb0de2964e6dc65f2f79ca90639ad1aaecf56
KoshyPNG <catalinmonmail@gmail.com>
2026-09-21 09:40:49 +0200

    Create display.go

---
+++ display.go
@@ -0,0 +1,26 @@
+package projet_red
+
+import "fmt"
+
+func DisplayInfo(perso character) {
+	fmt.Println("==========================")
+	fmt.Println("         CHARACTER        ")
+	fmt.Print("===========")
+	fmt.Print(perso.Nom)
+	fmt.Print("===========\n")
+	fmt.Print("Vie : ",perso.Vie_actuel)
+	fmt.Println("/",perso.Vie_max)
+	fmt.Println(perso.Piece,"pièce d'or")
+	fmt.Println("==========================")
+	var b float64
+	var c int
+	a := perso.Level
+	for i := 0;a >= 1;i++ {
+		a--
+		c = i
+	}
+	b = a*100.0
+	fmt.Println("Niveau", c)
+	fmt.Println(b,"% du niveau")
+	fmt.Println("==========================")
+}
